1a4ac362fb7abff81cf8096454bb3eca7b254187
Tomar jannu <meherazchisty57@gmail.com>
2024-12-16 23:58:05 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -2,7 +2,7 @@ module.exports = {
  config: {
 	 name: "prefix",
 	 version: "1.0",
-	 author: "Tokodori_Frtiz",//remodified by cliff
+	 author: "Meheraz",//remodified by cliff
 	 countDown: 5,
 	 role: 0,
 	 shortDescription: "no prefix",
@@ -15,12 +15,12 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 _<?>_ 𓆪 ]\n
+Yo, my prefix is [ 𓆩 _<#>_ 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥ &help [number of page] -> see commands
 ➥ &sim [message] -> talk to bot
 ➥ &callad [message] -> report any problem encountered
-➥ &help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/BAYJID.500k`,
+➥ &help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: MEHERAZ ISLAM CHISTY`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/I8JSUWI.jpeg")
  });
  }
