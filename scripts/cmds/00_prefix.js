1ec8bc4b02626fecd02335916605f277271ee4e9
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-01-04 16:25:10 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -21,7 +21,7 @@ Yo, my prefix is [ 𓆩 _<#>_ 𓆪 ]\n
 ➥ &sim [message] -> talk to bot
 ➥ &callad [message] -> report any problem encountered
 ➥ &help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: MEHERAZ ISLAM CHISTY`,
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/I8JSUWI.jpeg")
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/ccN3O9s.jpeg")
  });
  }
  }
